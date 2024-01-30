c67d159b4a2c158fd167ac3a2cb6983e1477db1d
Edward Oo <edwardoo@edwardoo-kkcompany-8.local>
2024-01-30 22:10:24 +0800

    feat: init 6-2-1 oidc-bucket

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-resource "aws_s3" "sre-m6-oidc-bucket-asdf" {
+resource "aws_s3_bucket" "sre-m6-oidc-bucket-asdf" {
     bucket = variable.bucket_name
     acl    = "private"
     tags = {
