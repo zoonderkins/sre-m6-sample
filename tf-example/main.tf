8004f9a4e325dc4d22208aa9a2761ab6f04ef5f7
Edward Oo <edwardoo@edwardocompany8.lan>
2024-01-31 15:46:15 +0800

    feat: init 6-2-1 oidc-bucket

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -11,10 +11,10 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "sre-m6-oidc-bucket-asdf" {
-    bucket = "sre-m6-test-bbbb"
+resource "aws_s3_bucket" "sre-m6-oidc-bucket-123412341234" {
+    bucket = "sre-m6-test-12341234"
     acl    = "private"
     tags = {
-        Name = "sre-m6-oidc-bucket-asdf"
+        Name = "sre-m6-oidc-bucket-asdfasdf"
     }
 }
